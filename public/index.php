5afce8b5ad5eb8b53f6f356ffa099e4c4d1290ef
Ethereal <67971655+ethereal97@users.noreply.github.com>
2021-01-04 19:09:51 +0630

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'Off');
 
-require __DIR__.'/../vendor/autoload.php';
+// require __DIR__.'/../vendor/autoload.php';
 
 define('HTTP_HEADER_FORMAT', '/^HTTP_(.+)$/');
 
